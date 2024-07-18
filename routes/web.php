67377b55ff1c11cbce62169fc5086601f6b6737e
mehdi <m3hdi.trader@gmail.com>
2024-07-18 09:53:53 +0330

    Model creation and migration and category  was done

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [TodoController::class, 'index'])->name('todo.index');
+
+// category Route
+
+Route::get('/categroies/create', [CategoryController::class, 'create'])->name('categroy.create');
+Route::Post('/categroies', [CategoryController::class, 'store'])->name('categroy.store');
